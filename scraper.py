f8a11179f80d0e41b54a9514b95155fb921b6fef
zachtownleysmith <zachtownleysmith@yahoo.com>
2021-03-15 19:10:51 -0500

    Added Coding Comments

--- scraper.py
+++ scraper.py
@@ -2,13 +2,18 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+# Define scraper function to return one scraped get request from a generic URL
+
 
 def scraper(url):
     site_text = requests.get(url).text
+    # From inspecting random_company endpoint, company name, purposes, and other info are <li> elements
+    # find_all('li') will return a list of all li elements which can be looped over
     company_info = BeautifulSoup(site_text, 'lxml').find_all('li')
     company_name = ''
     company_purpose = ''
 
+    # loop over all li elements, store text containing Name: and Purpose to a pandas dataframe
     for info in company_info:
         if 'Name:' in info.text:
             company_name = info.text.replace('Name: ', '')
@@ -18,6 +23,8 @@ def scraper(url):
     return company_df
 
 
+# Define loop_scraper to call scraper() multiple times and append the result to a pandas df
+# Output result to csv file
 def loop_scraper(call_num, url):
     scraped_df = pd.DataFrame(data=None, columns=["Name", "Purpose"])
     for i in range(call_num):
@@ -25,6 +32,7 @@ def loop_scraper(call_num, url):
     scraped_df.to_csv('output.csv', index=False)
 
 
+# Testing Block, run 50 get requests and save company Name/Purpose to CSV file
 if __name__ == '__main__':
     loop_scraper(50, url='http://18.206.38.144:8000/random_company')
 
